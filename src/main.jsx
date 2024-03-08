0dc8fa78a7bd13be92f52a9cf0db016d301ef117
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-08 23:12:54 +1030

    testing Tailwind instal progress went smoothly

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import "animate.css";
+
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+// import { store } from "./store";
+// import { Provider } from "react-redux";
+
+
+const theme = extendTheme({
+
+  styles: {
+    global: {
+      html: {
+        height: "100%",
+        width: "100%",
+        overflowX: "hidden",
+        overflowY: "auto",
+
+      },
+      body: {
+       
+        height: "100%",
+        width: "100%",
+        overflowX: "hidden", // Hide horizontal scrollbar
+        overflowY: "auto",   // Show vertical scrollbar when needed
+      },
+    },
+  },
+});
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ChakraProvider theme={theme}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </ChakraProvider>
 )
+
+
+{/* <Provider store={store}>
+<App />
+</Provider> */}
