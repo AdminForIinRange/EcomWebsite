a59fee7a7b4fd0e23781487d065681259919057b
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-10 23:01:28 +1030

    Simple Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
 
-export default function App() {
-  return (
-    <div className='text-red-600 text-2xl'>App</div>
+// layouts and pages
+import RootLayout from "./layouts/RootLayout.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Auth from "./pages/Auth/Auth.jsx";
+
+
+// router and routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="Home" element={<Home />} />
+      <Route path="Auth" element={<Auth />} />
+    </Route>
   )
+);
+
+function App() {
+
+
+  return <RouterProvider router={router} />;
 }
+
+export default App;
