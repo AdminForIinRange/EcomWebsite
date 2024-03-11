bb346d1c94bf13aca2e3426b1965ac703c5a992a
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-11 21:37:49 +1030

    Establishing Navbar and icons needed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import {
+ 
+  Input,
+
+  HStack,
+
+  InputGroup,
+
+  Highlight,
+
+  Button,
+  Text,
+  ButtonGroup ,
+
+  VStack,
+
+  InputRightAddon,
+  Stack,
+
+} from "@chakra-ui/react";
+
+import {SearchIcon } from '@chakra-ui/icons'
+import { FaUser } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 export default function Navbar() {
+  
   return (
-    <div>Navbar</div>
+   <HStack  h={"100px"} w={"100%"} justify={'center'} align={"center"} >
+    
+    <InputGroup
+    
+    transition="transform, 0.3s ease-in-out, boxShadow 1s ease-in-out"
+    _hover={{
+      transform: "scale(1.01)",
+     
+    }}
+    w={"35%"}
+    h={"40px"}
+    size={"lg"}
+    variant="filled"
+    borderRadius={"20px"}
+    bgColor={"#F5F5F5"}
+  >
+    <Input
+      borderRadius={"20px"}
+      borderWidth={"0px"}
+      h={"100%"}
+      fontSize={"16px"}
+      placeholder="Topic of Interest"
+      borderRightRadius={"5px"}
+      transition="transform, 0.3s ease-in-out, boxShadow 1s ease-in-out"
+
+      
+    />
+   
+  </InputGroup>
+
+  <Button    borderLeftRadius={"0px"}     w={"10px"} >
+       <SearchIcon  />
+        </Button>
+<Stack flexDir={"row"}  ml={"50px"} gap={"40px"} w={"10px"} justify={"center"} align={"center"} >
+
+<ButtonGroup variant={"ghost"} colorScheme="transparent" bgColor={"transparent"}  >
+  <Button _hover={{transform: "scale(1.2)"}}>
+  <FaUser size={"21px"}/>
+  </Button>
+  <Button  _hover={{transform: "scale(1.2)"}}>
+  <FaHeart size={"21px"}/>
+  </Button>
+
+</ButtonGroup>
+
+</Stack>
+
+
+   </HStack>
   )
 }
