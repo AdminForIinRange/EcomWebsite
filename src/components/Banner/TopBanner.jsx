29d64e4619a7c3e6a0045fe01f2d71905db7ca79
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-16 18:51:43 +1030

    re-creating the TopBanner, and allowing it to hold products

--- src/components/Banner/TopBanner.jsx
+++ src/components/Banner/TopBanner.jsx
@@ -1,64 +1,63 @@
 import React from "react";
 import HeadphoneYellow from "../../assets/ProductBanner/HeadphoneYellow.png";
-import { Box, HStack, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, HStack, Text, VStack } from "@chakra-ui/react";
 import {
   responsiveHeading,
   responsiveSubheading,
   responsiveTitleBanner,
-  responsiveTopBanner
-
+  responsiveTopBanner,
 } from "../../data/GlobalPresets/Presets";
 import banner from "../../data/Banner/banner.json";
-
+import { FaShoppingCart } from "react-icons/fa";
 import ScatterWorkBanner from "../../assets/ProductBanner/ScatterWorkBanner.png";
 export default function TopBanner() {
-
-  const { TopBannerIntro } = banner
+  const { TopBannerIntro } = banner;
   return (
     <HStack
+    
+      h={responsiveTopBanner}
+      rounded={"2xl"}
+      bgSize={"cover"}
+      // bgImage={`https://source.unsplash.com/random/1920x1080?silk+white`}
+      bgColor={"green.200"}
+      bgPos={"center"}
+      bgRepeat={"no-repeat"}
+      p={"2%"}
+      justifyContent={""}
+      alignItems={"end"}
       align={"start"}
       justify={"center"}
       w={"100%"}
-      h={"100%"}
-      py={"5px"}
-     >
-      <Box w={"100%"} h={"100%"} >
-        <Box
-          w={"100%"}
-          h={responsiveTopBanner}
-          rounded={"2xl"}
-          bgSize={"cover"}
-          bgImage={`https://source.unsplash.com/random/1920x1080?silk&${Date.now()}`}
 
-          bgPos={"center"}
-          bgRepeat={"no-repeat"}
-          p={"2%"}
-
-          justifyContent={""}
-          alignItems={"end"}
+    >
+      <VStack
+        color={"white"}
+        w={"100%"}
+        h={"100%"}
+        align={"left"}
+        justify={"center"}
+        p={"3"}
+      >
+        <Text
+          textAlign={""}
+          fontSize={["30px", "30px", "40px", "50px", "60px"]}
+          fontFamily={"Raleway"}
+          fontWeight={"700"}
         >
-          <VStack
-            color={"white"}
-            w={"100%"}
-            h={"100%"}
-            align={"left"}
-            justify={"end"}
+          {TopBannerIntro[0].header}
+        </Text>
+        <Text fontSize={responsiveSubheading} fontFamily={"Raleway"}>
+          {TopBannerIntro[0].subheading}
+        </Text>
 
-          >
-            <Text textShadow={"0px 0px 10px #000000"}
-              textAlign={""}
-              fontSize={responsiveHeading}
-              fontFamily={"Raleway"}
-              fontWeight={"700"}
-            >
-              {TopBannerIntro[0].header}
-            </Text>
-            <Text fontSize={responsiveSubheading} fontFamily={"Raleway"}>
-              {TopBannerIntro[0].subheading}
-            </Text>
-          </VStack>
-        </Box>
-      </Box>
+        <Button leftIcon={<FaShoppingCart />} h={"50px"} w={"150px"}>
+          Add to Cart!
+        </Button>
+      </VStack>
+
+      <HStack bg={'green.300'} w={"100%"} h={"100%"} >
+
+      </HStack>
     </HStack>
   );
 }
